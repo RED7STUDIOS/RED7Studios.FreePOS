33d36bcbef3bdabc85f32066820c08bf6ee83964
Creaous <68230846+Creaous@users.noreply.github.com>
2020-09-04 18:16:18 +0930

    Working?

---
+++ RED7Studios.TMBApp/CreateOrder.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace RED7Studios.TMBApp
+{
+    partial class CreateOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_balance = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_paid = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_net = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_discount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_sub = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.txt_qty = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmb_items = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(395, 109);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 23);
+            this.button5.TabIndex = 48;
+            this.button5.Text = "Remove Item";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(218, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 47;
+            this.button4.Text = "Print";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(122, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 46;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Save.Location = new System.Drawing.Point(20, 378);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(96, 23);
+            this.btn_Save.TabIndex = 45;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(340, 378);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 44;
+            this.label9.Text = "Balance";
+            // 
+            // txt_balance
+            // 
+            this.txt_balance.Location = new System.Drawing.Point(395, 375);
+            this.txt_balance.Name = "txt_balance";
+            this.txt_balance.Size = new System.Drawing.Size(91, 20);
+            this.txt_balance.TabIndex = 43;
+            this.txt_balance.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(317, 352);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "Paid Amount";
+            // 
+            // txt_paid
+            // 
+            this.txt_paid.Location = new System.Drawing.Point(395, 349);
+            this.txt_paid.Name = "txt_paid";
+            this.txt_paid.Size = new System.Drawing.Size(91, 20);
+            this.txt_paid.TabIndex = 41;
+            this.txt_paid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(322, 326);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 40;
+            this.label7.Text = "Net Amount";
+            // 
+            // txt_net
+            // 
+            this.txt_net.Location = new System.Drawing.Point(395, 323);
+            this.txt_net.Name = "txt_net";
+            this.txt_net.Size = new System.Drawing.Size(91, 20);
+            this.txt_net.TabIndex = 39;
+            this.txt_net.Text = "0";
+            this.txt_net.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(168, 323);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Discount";
+            // 
+            // txt_discount
+            // 
+            this.txt_discount.Location = new System.Drawing.Point(225, 320);
+            this.txt_discount.Name = "txt_discount";
+            this.txt_discount.Size = new System.Drawing.Size(90, 20);
+            this.txt_discount.TabIndex = 37;
+            this.txt_discount.Text = "0";
+            this.txt_discount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Sub Total";
+            // 
+            // txt_sub
+            // 
+            this.txt_sub.Location = new System.Drawing.Point(81, 320);
+            this.txt_sub.Name = "txt_sub";
+            this.txt_sub.Size = new System.Drawing.Size(86, 20);
+            this.txt_sub.TabIndex = 35;
+            this.txt_sub.Text = "0";
+            this.txt_sub.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(20, 154);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(466, 160);
+            this.listView1.TabIndex = 34;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Item";
+            this.columnHeader1.Width = 230;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Qty";
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total";
+            this.columnHeader4.Width = 80;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(395, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Add Item";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(309, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Total";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(182, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Qty";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Price";
+            // 
+            // txt_total
+            // 
+            this.txt_total.Location = new System.Drawing.Point(276, 83);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(100, 20);
+            this.txt_total.TabIndex = 29;
+            this.txt_total.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_total.TextChanged += new System.EventHandler(this.txt_total_TextChanged);
+            // 
+            // txt_qty
+            // 
+            this.txt_qty.Location = new System.Drawing.Point(147, 82);
+            this.txt_qty.Name = "txt_qty";
+            this.txt_qty.Size = new System.Drawing.Size(100, 20);
+            this.txt_qty.TabIndex = 28;
+            this.txt_qty.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_qty.TextChanged += new System.EventHandler(this.txt_qty_TextChanged);
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(26, 83);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(90, 20);
+            this.txt_price.TabIndex = 27;
+            this.txt_price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Select Item";
+            // 
+            // cmb_items
+            // 
+            this.cmb_items.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_items.FormattingEnabled = true;
+            this.cmb_items.Items.AddRange(new object[] {
+            "Blue",
+            "Red",
+            "White",
+            "Yellow",
+            "Black",
+            "Sweets Item 1"});
+            this.cmb_items.Location = new System.Drawing.Point(108, 10);
+            this.cmb_items.Name = "cmb_items";
+            this.cmb_items.Size = new System.Drawing.Size(364, 24);
+            this.cmb_items.TabIndex = 25;
+            this.cmb_items.SelectedIndexChanged += new System.EventHandler(this.cmb_items_SelectedIndexChanged);
+            // 
+            // CreateOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 410);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_balance);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_paid);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_net);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_discount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_sub);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.txt_qty);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb_items);
+            this.Name = "CreateOrder";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CreateOrder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_balance;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_paid;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_net;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_discount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_sub;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_total;
+        private System.Windows.Forms.TextBox txt_qty;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_items;
+    }
+}
+
---
+++ RED7Studios.TMBApp/CreateOrder.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+// DB INFO:
+// IP: 52.187.233.224
+// Username : macarons_storeapp
+// Password : Vf7gd5*3
+// Database : macarons_storeapp
+
+namespace RED7Studios.TMBApp
+{
+    public partial class CreateOrder : Form
+    {
+        string databaseConnection = "Server=52.187.233.224;Database=macarons_storeapp;Uid=macarons_storeapp;Pwd=Vf7gd5*3;";
+
+        MySqlConnection conn = new MySqlConnection("Server = 52.187.233.224; Database=macarons_storeapp;Uid=macarons_storeapp;Pwd=Vf7gd5*3;");
+
+        public CreateOrder()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void CreateOrder_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] arr = new string[4];
+            arr[0] = cmb_items.SelectedItem.ToString();
+            arr[1] = txt_price.Text;
+            arr[2] = txt_qty.Text;
+            arr[3] = txt_total.Text;
+
+            ListViewItem lvi = new ListViewItem(arr);
+            listView1.Items.Add(lvi);
+
+
+            txt_sub.Text = (Convert.ToInt16(txt_sub.Text) + Convert.ToInt16(txt_total.Text)).ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    if (listView1.Items[i].Selected)
+                    {
+                        txt_sub.Text = (Convert.ToInt16(txt_sub.Text) - Convert.ToInt16(listView1.Items[i].SubItems[3].Text)).ToString();
+                        listView1.Items[i].Remove();
+
+
+                    }
+                }
+            }
+        }
+
+        private void txt_qty_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_qty.Text.Length > 0)
+            {
+                txt_total.Text = (Convert.ToInt16(txt_qty.Text) * Convert.ToInt16(txt_price.Text)).ToString();
+            }
+        }
+
+        private void cmb_items_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmb_items.SelectedItem.ToString() == "Sweets Item 1")
+            { txt_price.Text = "50"; }
+            else if (cmb_items.SelectedItem.ToString() == "Sweets Item 2")
+            { txt_price.Text = "100"; }
+            else if (cmb_items.SelectedItem.ToString() == "Sweets Item 3")
+            { txt_price.Text = "150"; }
+            else if (cmb_items.SelectedItem.ToString() == "Bakery Item 1")
+            { txt_price.Text = "200"; }
+            else if (cmb_items.SelectedItem.ToString() == "Bakery Item 2")
+            { txt_price.Text = "250"; }
+            else if (cmb_items.SelectedItem.ToString() == "Bakery Item 3")
+            { txt_price.Text = "300"; }
+            else
+            { txt_price.Text = "0"; }
+
+
+            txt_total.Text = "";
+            txt_qty.Text = "";
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                try
+                {
+                    int total = Convert.ToInt16(txt_sub.Text) - Convert.ToInt16(txt_discount.Text);
+
+                    string Query = "Insert into invoice_master (date, total, discount, item, itemprice, itemqty, itemtotal) values " +
+                       "(NOW(), " + total + ", " + txt_discount.Text + ", " + cmb_items.Text + ", " + txt_price.Text + ", " + txt_qty.Text + ", " + txt_total.Text + ")";
+                    //This is  MySqlConnection here i have created the object and pass my connection string.  
+                    //This is command class which will handle the query and connection object.  
+                    MySqlCommand command = new MySqlCommand(Query, conn);
+                    MySqlDataReader reader;
+                    conn.Open();
+                    reader = command.ExecuteReader();     // Here our query will be executed and data saved into the database.  
+                    MessageBox.Show("Save Data");
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occured: " + ex.Message, "CRITICAL ERROR!");
+                }
+            }
+        }
+
+        private void txt_total_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
